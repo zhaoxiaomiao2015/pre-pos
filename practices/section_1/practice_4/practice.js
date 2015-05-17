c05efb074c82d1340a422688e3a7b7ebac48be77
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-17 17:18:16 +0800

    three commit

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,13 @@
-function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+function collect_same_elements(collection_a, collection_b) {
+  var result =[];
+   for(var x=0;x< collection_a.length;x++){
+         for( var y=0; y< collection_b.value.length; y++){
+           if(collection_a[x].key ==  collection_b.value[y] ){
+         result.push ( collection_a[x].key );
+           }
+       }
+    }
+    return   result;
 }
 
 module.exports = collect_same_elements;
