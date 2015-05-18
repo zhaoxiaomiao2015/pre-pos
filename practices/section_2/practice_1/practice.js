582d86446f0d95b0114bd1ed4385a8333dca108b
zhaoxiaomiao2015 <micsoftzhaoxiaomiao@outlook.com>
2015-05-18 15:25:19 +0800

    change sema[] to result[]

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,24 +1,24 @@
-function count_same_elements(  collection) {
-  var seam=[];
-  //循环取出不同的元素
-  for(var i = 0; i <  collection.length; i++){
-    var isExist = false;//是否找到标示
-    for(var j = 0;   j <  seam.length; j++ ){
-      //  如果存在则count++
-      if( collection[i] === seam[j].key){
-          seam[j].count  ++;
+function count_same_elements(collection) {
+  var result = [];
+  //循环遍历所给数组
+  for( var i = 0; i < collection.length; i++){
+    var isExist = false;
+    //与结果数组中对比如果存在，则count++
+    for(var j = 0; j < result.length; j++){
+      if(collection[i] === result[j].key){
+          result[j].count++;
           isExist = true;
-          }
       }
-      if(!isExist){ //则说明是个新元素，需要创建
-        var temp = {};
-              temp.key = collection[i];
-              temp.count = 1;
-              //建立结果类型数组
-              seam.push(temp);
-            }
+    }
+    if(!isExist){
+      //与结果数组中对比如果不在，则在结果数组追加
+      var temp = {};
+      temp.key = collection[i];
+      temp.count = 1;
+      result.push(temp);
+    }
   }
-  return seam;
+  return result;
 }
 
 module.exports = count_same_elements;
